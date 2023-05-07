a21926b9d6c4219c88dfd3696a914361567a38c6
Sasha <savylovskij@gmail.com>
2023-05-07 23:27:58 +0300

    remove unnecessary code

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,21 +1,3 @@
 export const environment = {
   production: false,
-
-  tokenEP: 'eyJuYW1lIjoiR0tJbnRlbHNlcnYiLCJwYXNzd29yZCI6IkVQIn0',
-  urlPostEP: 'https://medoc.net.ua/api_3_0',
-  urlGetEP: 'https://medoc.net.ua/api_GET',
-
-  tokenSite: 'eyJuYW1lIjoiR0tJbnRlbHNlcnYiLCJwYXNzd29yZCI6IkludGVsc2VydiJ9',
-  urlPostSite: 'https://res.intelserv.net.ua/api_3_0',
-  urlGetSite: 'https://res.intelserv.net.ua/api_GET',
-
-  urlSiteUploadFile: 'https://res.intelserv.net.ua/uploadFile',
-  urlEpUploadBidFile: 'https://medoc.net.ua/uploadBidFile',
-
-  // Токен создавал на dev.intelserv@gmail.com
-  tokenGoogle:
-    '590108774232-6b0j3gr7id8s2033vaqgdc1rj70atdef.apps.googleusercontent.com',
-  tokenFacebook: '236683287882059',
-
-  serverPath: 'dist/ng-intelserv/browser',
 };

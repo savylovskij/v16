6e2acef2cbc7e320426d187452d53b4594913072
Sasha <savylovskij@gmail.com>
2024-09-15 22:19:52 +0300

    add first test

---
+++ src/app/modules/posts/services/posts.service.spec.ts
@@ -0,0 +1,71 @@
+import { TestBed } from '@angular/core/testing';
+
+import { finalize, of, take, tap, throwError } from 'rxjs';
+
+import { PostRestService } from './post-rest.service';
+import { PostsService } from './posts.service';
+import { Post } from '../models';
+
+describe('PostsService', () => {
+  let service: PostsService;
+  let postRestServiceSpy: jasmine.SpyObj<PostRestService>;
+
+  beforeEach(() => {
+    const postRestServiceMock = jasmine.createSpyObj('PostRestService', [
+      'getPosts',
+    ]);
+
+    TestBed.configureTestingModule({
+      providers: [
+        PostsService,
+        { provide: PostRestService, useValue: postRestServiceMock },
+      ],
+    });
+
+    service = TestBed.inject(PostsService);
+    postRestServiceSpy = TestBed.inject(
+      PostRestService,
+    ) as jasmine.SpyObj<PostRestService>;
+  });
+
+  it('should set initialize isLoading false', () => {
+    expect(service.isLoading()).toBeFalse();
+  });
+
+  it('should set isLoading to true when postsList$ is subscribed and false after completion', () => {
+    postRestServiceSpy.getPosts.and.returnValue(of([]));
+
+    service.postsList$
+      .pipe(
+        tap(() => expect(service.isLoading()).toBeTrue()),
+        take(1),
+        finalize(() => expect(service.isLoading()).toBeFalse()),
+      )
+      .subscribe();
+  });
+
+  it('should handle errors and update isLoading signal', () => {
+    postRestServiceSpy.getPosts.and.returnValue(
+      throwError(() => new Error('Network error')),
+    );
+
+    service.postsList$
+      .pipe(
+        take(1),
+        finalize(() => expect(service.isLoading()).toBeFalse()),
+      )
+      .subscribe();
+  });
+
+  it('should receive posts data', () => {
+    const mockPosts = [
+      { id: 1, title: 'Post 1' },
+      { id: 2, title: 'Post 2' },
+    ] as Post[];
+    postRestServiceSpy.getPosts.and.returnValue(of(mockPosts));
+
+    service.postsList$
+      .pipe(take(1))
+      .subscribe(result => expect(result).toEqual(mockPosts));
+  });
+});

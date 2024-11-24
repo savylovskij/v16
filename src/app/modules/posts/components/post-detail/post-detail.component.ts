8079320de0aae81b9f330cbdf96d9480a97376aa
Sasha <savylovskij@gmail.com>
2024-11-24 15:41:33 +0200

    refactor: rename values

--- src/app/modules/posts/components/post-detail/post-detail.component.ts
+++ src/app/modules/posts/components/post-detail/post-detail.component.ts
@@ -1,19 +1,19 @@
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 
-import { RandomRangePipe } from '@app/shared/pipes/random-range';
-import { RepeatDirective } from '@app/shared/directives/repeat';
 import { SkeletonComponent } from '@app/shared/components/skeleton';
+import { RepeatDirective } from '@app/shared/directives/repeat';
+import { RandomRangePipe } from '@app/shared/pipes/random-range';
 
 import { Post } from '../../models';
 
 @Component({
-    selector: 'app-post-detail',
-    templateUrl: './post-detail.component.html',
-    styleUrls: ['./post-detail.component.scss'],
-    changeDetection: ChangeDetectionStrategy.OnPush,
-    imports: [RandomRangePipe, RepeatDirective, SkeletonComponent]
+  selector: 'app-post-detail',
+  templateUrl: './post-detail.component.html',
+  styleUrls: ['./post-detail.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [RandomRangePipe, RepeatDirective, SkeletonComponent],
 })
 export class PostDetailComponent {
   public readonly post = input<Post | null>(null);
-  public readonly loading = input(false);
+  public readonly isLoading = input(false);
 }
--- src/app/modules/posts/components/posts-list/posts-list.component.ts
+++ src/app/modules/posts/components/posts-list/posts-list.component.ts
@@ -1,27 +1,26 @@
 import {
   ChangeDetectionStrategy,
   Component,
-  EventEmitter,
   input,
-  Output,
+  output,
 } from '@angular/core';
 
-import { RandomRangePipe } from '@app/shared/pipes/random-range';
-import { RepeatDirective } from '@app/shared/directives/repeat';
 import { SkeletonComponent } from '@app/shared/components/skeleton';
+import { RepeatDirective } from '@app/shared/directives/repeat';
+import { RandomRangePipe } from '@app/shared/pipes/random-range';
 
 import { Post } from '../../models';
 
 @Component({
-    selector: 'app-posts-list',
-    templateUrl: './posts-list.component.html',
-    styleUrls: ['./posts-list.component.scss'],
-    changeDetection: ChangeDetectionStrategy.OnPush,
-    imports: [RandomRangePipe, RepeatDirective, SkeletonComponent]
+  selector: 'app-posts-list',
+  templateUrl: './posts-list.component.html',
+  styleUrls: ['./posts-list.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [RandomRangePipe, RepeatDirective, SkeletonComponent],
 })
 export class PostsListComponent {
   public readonly postsLis = input<Post[]>([]);
-  public readonly loading = input(false);
+  public readonly isLoading = input(false);
 
-  @Output() public selectedPost = new EventEmitter<number>();
+  public selectedPost = output<number>();
 }
